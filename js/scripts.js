c3281734862e035d58dfdd445cbce6bbd509201b
Hayden <116609248+haydenmurphey@users.noreply.github.com>
2024-09-26 19:12:02 -0400

    project 2 slideshow added

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,34 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+
+   // Remove "active" class from all slides
+   for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+
+  // Add "active" class to the current slide and dot
+  slides[slideIndex - 1].classList.add("active");
+}
+
